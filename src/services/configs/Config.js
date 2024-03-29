9dd158a7a12de985d60c19b1018be0fee3b1121b
Ali.r <alirahimi611@gmail.com>
2024-03-30 02:42:08 +0430

    trying to fix cors bug on vercel

--- src/services/configs/Config.js
+++ src/services/configs/Config.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 export const app = axios.create({
-  baseURL: process.env.NODE_ENV == "development"?  "http://localhost:3000/api": "https://tarkhine-rjbqkq88b-alidevr1996s-projects.vercel.app/api",
+  baseURL: process.env.NODE_ENV == "development"?  "http://localhost:3000/api": "https://www.tarkhine-rjbqkq88b-alidevr1996s-projects.vercel.app/api",
   withCredentials: true,
 });
 
